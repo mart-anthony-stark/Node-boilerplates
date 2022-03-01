52769ddcd9bc5ab2e096a8b8e68ae63daef00f7e
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-01 21:43:28 +0800

    delete

--- src/routes/home.route.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import homeController from "../controller/home.controller";
-const router = Router();
-
-router.get("/", homeController.getHome);
-
-export default router;
