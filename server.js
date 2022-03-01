a68f36e2bb5fa3e544aa20ed046a054537b4fc3c
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-01 19:26:12 +0800

    init app

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const init = require("./utils/init");
 
 const app = express();
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello express");
 });
+
+init(app);
