77dbb3357c79ef25a11055761ca2c342941ce9e5
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-01 20:18:34 +0800

    add initial create user

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -1,7 +1,16 @@
 const User = require("../models/User.model");
+const bcrypt = require("bcryptjs");
 
 module.exports = {
-    signup: async(req,res) => {
-        
+  signup: async (req, res) => {
+    try {
+      const user = new User(req.body);
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(req.password, salt);
+
+      await user.save();
+    } catch (error) {
+      res.status(500).send(error);
     }
-}
+  },
+};
