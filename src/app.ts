92ac352274d46fb1891b2f2b688a0fd819ac32c0
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-01 21:43:20 +0800

    add post router

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import express, { Application } from "express";
 import init from "./utils/init";
 import * as dotenv from "dotenv";
-import homeRouter from "./routes/home.route";
+import postsRouter from "./routes/posts.route";
 dotenv.config({});
 
 const app: Application = express();
@@ -9,6 +9,6 @@ const app: Application = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.use("/", homeRouter);
+app.use("/posts", postsRouter);
 
 init(app);
