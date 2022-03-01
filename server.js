e90502a97fc334afc8b9f6b76d7149e643ea5754
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-01 19:28:25 +0800

    add middlewares

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const init = require("./utils/init");
 
 const app = express();
 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Hello express");
 });
