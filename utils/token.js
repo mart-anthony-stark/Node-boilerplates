3242734e08a995961eceddc8534d37c6aa313c9c
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-01 20:46:59 +0800

    add verifyToken

--- utils/token.js
+++ utils/token.js
@@ -6,6 +6,20 @@ const createToken = (body) => {
   });
 };
 
+const verifyToken = (req, res, next) => {
+  const authHeader = req.headers.token;
+  if (!authHeader) return res.status(403).send({ error: "Unauthenticated" });
+
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) return res.status(403).send({ error: "Invalid Token" });
+
+    req.user = user.user;
+    next();
+  });
+};
+
 module.exports = {
   createToken,
+  verifyToken,
 };
