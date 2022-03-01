572003596b2b678a992b95a6a9fac5dacb88372e
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-01 21:09:31 +0800

    delete

--- src/utils/init.js
+++
@@ -1,9 +0,0 @@
-const init = (app) => {
-  const PORT = process.env.PORT || 8080;
-  app.listen(PORT, (e) => {
-    if (e) return console.log(e);
-    console.log(`Server running on port ${PORT}`);
-  });
-};
-
-module.exports = init;
