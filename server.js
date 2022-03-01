19162a481f2c0ce1cb43d2cd35ec8964784aabd7
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-01 19:26:34 +0800

    remove port

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const init = require("./utils/init");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello express");
