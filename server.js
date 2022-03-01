9872435ba16918e93e95c39ee232fd11117d9281
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-01 20:35:56 +0800

    add use route

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+app.use("/auth", require("./routes/auth.route"));
 app.use("/posts", require("./routes/posts.route"));
 
 init(app);
