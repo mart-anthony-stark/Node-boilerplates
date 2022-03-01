d4b4cb5d3843262d2e6d39af4dd23e88fa966712
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-01 19:48:34 +0800

    add logs

--- utils/init.js
+++ utils/init.js
@@ -1,16 +1,17 @@
 const mongoose = require("mongoose");
 
-const init = (app) => {
+const init = async (app) => {
   const PORT = process.env.PORT || 8080;
   app.listen(PORT, (e) => {
     if (e) return console.log(e);
     console.log(`Server running on port ${PORT}`);
   });
 
-  mongoose.connect(process.env.DB_URI, {
+  await mongoose.connect(process.env.DB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
+  console.log("Connected to database");
 };
 
 module.exports = init;
