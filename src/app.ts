b9f42b5d50f4298f779bd9d0af9e9c01b7a43be8
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-01 21:18:29 +0800

    add home router

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,13 @@
 import express from "express";
 import init from "./utils/init";
 import * as dotenv from "dotenv";
+import homeRouter from "./routes/home.route";
 dotenv.config({});
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.use("/", require("./routes/home.route"));
+app.use("/", homeRouter);
 
 init(app);
