4d2aa104e659fa9c86ec4549836a28630d9cc65f
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-01 21:01:40 +0800

    moved to src

---
+++ src/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const init = require("../utils/init");
+require("dotenv").config({});
+
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use("/", require("../routes/home.route"));
+
+init(app);
