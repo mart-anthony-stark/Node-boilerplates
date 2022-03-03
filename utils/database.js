2f60d4699bba00540df52185a0ca652c47e018ec
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-03 15:01:55 +0800

    change db

--- utils/database.js
+++ utils/database.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2");
 const connection = mysql.createPool({
   host: "localhost",
   user: "root",
-  database: "test",
+  database: "school_management",
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
