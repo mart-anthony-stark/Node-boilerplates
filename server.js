84377c70257a1aca50ddda398795da2ab6932060
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-03 15:01:48 +0800

    add admin route

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const init = require("./utils/init");
+const db = require("./utils/database");
 require("dotenv").config({});
 
 const app = express();
@@ -8,5 +9,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use("/", require("./routes/home.route"));
+app.use("/admin", (req, res) => {
+  db.query("SELECT * from admin", (err, rows) => {
+    if (err) console.log(err);
+    console.log(rows);
+    res.send(rows);
+  });
+});
 
 init(app);
