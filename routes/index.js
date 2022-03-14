d50cfad3773afb0e2af358b15d423ce809f19080
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-14 15:12:52 +0800

    add log

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,12 @@
 const controller = require("../controllers");
 
+const log = (req, res, next) => {
+  console.log("sdf");
+  next();
+};
+
 const routes = (fastify, options, done) => {
-  fastify.get("/", controller.getItems);
+  fastify.get("/", { preHandler: [log] }, controller.getItems);
   done();
 };
 
