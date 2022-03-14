c6a3e55df2632778e23dda7fd1d0b45754981c4d
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-14 15:02:00 +0800

    remove route

--- server.js
+++ server.js
@@ -1,22 +1,6 @@
 const fastify = require("fastify")({ logger: true });
 
-fastify.route({
-  method: "GET",
-  url: "/",
-  schema: {
-    // request needs to have a querystring with a `name` parameter
-    querystring: {
-      name: { type: "string" },
-    },
-  },
-  // this function is executed for every request before the handler is executed
-  preHandler: async (request, reply) => {
-    // E.g. check authentication
-  },
-  handler: async (request, reply) => {
-            reply.send({ asd: "asd" });
-  },
-});
+
 
 const start = async () => {
   try {
