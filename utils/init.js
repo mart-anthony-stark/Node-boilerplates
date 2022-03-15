3b3d93948c48e23b3326a0aa8474b2f0ada1c036
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-15 20:38:28 +0800

    add mongoose

--- utils/init.js
+++ utils/init.js
@@ -1,9 +1,12 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const start = async (fastify) => {
   try {
     await fastify.listen(3000);
-    await mongoose.connect(process.env.DB_URI, )
+    await mongoose.connect(process.env.DB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
