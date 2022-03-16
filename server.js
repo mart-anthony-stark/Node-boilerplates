05dec47b07dd1a5a196ab8690a274e42891f49e0
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-16 17:02:22 +0800

    delete

--- server.js
+++
@@ -1,7 +0,0 @@
-const fastify = require("fastify")({ logger: true });
-const start = require("./utils/init");
-require('dotenv').config({})
-
-fastify.register(require("./routes"));
-
-start(fastify);
