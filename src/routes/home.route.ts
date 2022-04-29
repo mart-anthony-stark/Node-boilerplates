5d92e7513c8e5593898606a4ff55d06c4d1badd1
mart-anthony-stark <martsalazar0@gmail.com>
2022-04-29 20:31:04 +0800

    Add: catcher util

--- src/routes/home.route.ts
+++ src/routes/home.route.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
 import homeController from "../controller/home.controller";
+import { catcher } from "../utils";
 const router = Router();
 
-router.get("/", homeController.getHome);
+router.get("/", catcher(homeController.getHome));
 
 export default router;
