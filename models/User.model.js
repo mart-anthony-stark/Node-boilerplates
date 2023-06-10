b9a5ff8f698fdad5668ca1c0cf65b81b18bdac6f
mart-anthony-stark <martsalazar0@gmail.com>
2023-06-10 20:43:36 +0800

    Add: mongodb check if not unique

--- models/User.model.js
+++ models/User.model.js
@@ -2,12 +2,22 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema(
   {
-    username: { type: String, required: true, unique: true },
+    username: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true, minlength: 8 },
     role: { type: String, default: "user", enum: ["user", "admin"] },
   },
   { timestamps: true }
 );
+
+userSchema.post("save", function (error, doc, next) {
+  if (error.name === "MongoServerError" && error.code === 11000) {
+    next(
+      new Error("Email address already in use. Please enter different email.")
+    );
+  } else {
+    next();
+  }
+});
 
 module.exports = mongoose.model("User", userSchema);
