2bd32e5e301b333210d3349bc680b9cbfaed0f51
mart-anthony-stark <martsalazar0@gmail.com>
2023-06-10 20:43:57 +0800

    Fixed catcher export

--- utils/helper.js
+++ utils/helper.js
@@ -4,7 +4,7 @@
  * @returns {Promise}
  * Automatically handles and catches errors in application (sends error message to client)
  */
-export const catcher = (fn, cb) => (request, response) => {
+exports.catcher = (fn, cb) => (request, response) => {
   Promise.resolve(fn(request, response)).catch((err) => {
     if (cb) {
       cb(request, response);
