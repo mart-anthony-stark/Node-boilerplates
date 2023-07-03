d0642327e50d01e2c4df0a9d8db4ee326f757d91
mart-anthony-stark <martsalazar0@gmail.com>
2023-07-03 21:47:07 +0800

    delete files

--- src/routes/posts.route.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import postController from "../controllers/post.controller";
-const router = Router();
-
-router.get("/", postController.getAllPosts);
-
-export default router;
