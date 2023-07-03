55af25b4b1608abcc44b263096f5791ec4054a18
mart-anthony-stark <martsalazar0@gmail.com>
2023-07-03 22:21:36 +0800

    Add: authguard

---
+++ src/guards/AuthGuard.ts
@@ -0,0 +1,44 @@
+import { NextFunction, Request, Response } from "express";
+import { IUserRequest } from "../types";
+const jwt = require("jsonwebtoken");
+
+interface IJwtError {
+  message: string;
+}
+interface IJwtDecodedToken {
+  body: Object;
+}
+
+const AuthGuard = (req: IUserRequest, res: Response, next: NextFunction) => {
+  try {
+    const authHeader = req.headers.authorization;
+    if (!authHeader)
+      return res
+        .status(401)
+        .send({ message: "Authorization token required! Please login first" });
+
+    const token = authHeader.split(" ")[1];
+    jwt.verify(
+      token,
+      process.env.JWT_SECRET,
+      async (err: IJwtError, decoded: IJwtDecodedToken) => {
+        const body = decoded?.body;
+        if (!body || err) {
+          return res.status(401).send({
+            message: "Request is not authorized! Please login first",
+            error: err.message,
+          });
+        }
+
+        req.user = body;
+        req.token = token;
+        next();
+      }
+    );
+  } catch (error: any) {
+    res.status(401).send({
+      message: "Request is not authorized! Please login first",
+      error: error.message,
+    });
+  }
+};
--- src/types/Request.types.ts
+++ src/types/Request.types.ts
@@ -2,3 +2,8 @@ import { Request } from "express";
 export interface IGetUserAuthInfoRequest extends Request {
   user: any; // or any other type
 }
+
+export interface IUserRequest extends Request {
+  user: unknown;
+  token: string;
+}
